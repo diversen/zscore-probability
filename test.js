7120bf6677a13b6cc55843cc55326e375baa4e7f
Dennis Iversen <dennis.iversen@gmail.com>
2021-08-08 11:32:06 +0200

    some more examples

--- test.js
+++ test.js
@@ -71,3 +71,51 @@ console.log('z score lightest', z_score_lightest)
 let x = 172 + 29 * z_score_lightest;
 console.log('weight that separates the lightest from the heaviest', x)
 // -> 246.6990522146225 
+
+/**
+ * 
+ * sample proportion of the mean
+ * 
+ * Body temp. has an average of 98.5
+ * Std deviation is 0.62
+ * 
+ * A sample of 106 people. What is the probability that the 
+ * average temp. for the sample will be 98.2 or lower
+ * 
+ * (x_bar - mean) / (sigma/ sqrt n) 
+ */
+let z_score_temp = (98.2 - 98.6) / (0.62 / Math.sqrt(106))
+console.log('z score temperature', z_score_temp)
+console.log('Probability is ', zScoreToProbability(z_score_temp))
+
+/**
+ * confidence intervals for the population proportion
+ * 
+ * 280 trials, 123 corrrect
+ * p_hat = 123/280
+ * q_hat = 1 - p_hat
+ * 
+ * alha_level = 0.05
+ * confidence = 0.95
+ * 
+ * E = probabilityToZscore(0.95 + alpha_level / 2) * Math.sqrt((133/280)*(1-133/280)/280 
+ * 
+ */
+
+// 
+let p_hat = 123/280
+
+let E = probabilityToZscore(0.975) * Math.sqrt(p_hat*(1-p_hat)/280)
+let Etable = 1.96 * Math.sqrt(p_hat*(1-p_hat)/280)
+
+
+console.log('z-score using table', 1.96)
+// -> 1.96
+console.log('z-score using zScoreToProbability', probabilityToZscore(0.975))
+// -> 1.9599641561508179
+
+console.log('Using zScoreToProbability. You are 95 confident that the population is in the interval between ', p_hat - E, p_hat + E ) 
+// -> 0.3811539553457276 0.49741747322570096
+
+console.log('Using table lookup You are 95 confident that the population is in the interval between ', p_hat - Etable, p_hat + Etable ) 
+// -> 0.3811528922313877 0.49741853634004085
